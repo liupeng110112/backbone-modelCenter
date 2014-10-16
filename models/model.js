de62b8511fd77af63dc2018670fbed0f4a9d30e5
刘鹏 <1403519825@qq.com>
2014-10-16 09:59:00 +0800

    Create model.js

---
+++ models/model.js
@@ -0,0 +1,11 @@
+var Model = Backbone.Model.extend({
+    defaults : {
+      title: '',
+      author: '',
+      downloadTime: 0,
+      comment: 0,
+      grade: 0,
+      fav: false,
+      content:''
+    }
+})
